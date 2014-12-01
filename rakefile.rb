28b53bd9baff429b84be3b86a9d9c6699009fa57
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2014-12-01 17:55:50 -0500

    Albacore upgrade

--- rakefile.rb
+++ rakefile.rb
@@ -1,16 +1,17 @@
+Configuration = 'Release'
+
 COPYRIGHT = "Copyright 2013 Eugene Tolmachev, All rights reserved."
-
-include FileTest
-require 'albacore'
-require 'semver'
-
+DESCRIPTION = 'Bitlore, minimalist validation library for DDD affectionados'
 PRODUCT = 'Bitlore'
-CLR_TOOLS_VERSION = 'v4.0.30319'
-OUTPUT_PATH = 'bin/Release'
+
+require 'albacore'
+require 'albacore/tasks/versionizer'
+require 'semver'
+include FileTest
 
 props = {
   :src => File.expand_path("src"),
-  :output => File.expand_path("build_output"),
+  :out => File.expand_path("build_output"),
   :artifacts => File.expand_path("build_artifacts"),
   :projects => ["Bitlore"],
   :lib => File.expand_path("lib"),
@@ -23,48 +24,86 @@ task :all => [:default, :package]
 desc "**Default**, compiles and runs tests"
 task :default => [:clean, :nuget_restore, :compile, :tests, :package]
 
-desc "Update the common version information for the build. You can call this task without building."
-assemblyinfo :global_version do |asm|
-  # Assembly file config
-  asm.product_name = PRODUCT
-  asm.description = "Bitlore, minimalist validation library"
-  asm.version = FORMAL_VERSION
-  asm.file_version = FORMAL_VERSION
-  asm.custom_attributes :AssemblyInformationalVersion => "#{BUILD_VERSION}",
-	:CLSCompliantAttribute => true
-  asm.copyright = COPYRIGHT
-  asm.output_file = 'src/SolutionVersion.cs'
-  asm.namespaces "System", "System.Reflection", "System.Runtime.InteropServices"
-end
-
 desc "Prepares the working directory for a new build"
 task :clean do
-	FileUtils.rm_rf props[:output]
-	waitfor { !exists?(props[:output]) }
+	FileUtils.rm_rf props[:out]
+	waitfor { !exists?(props[:out]) }
 
 	FileUtils.rm_rf props[:artifacts]
 	waitfor { !exists?(props[:artifacts]) }
 
-	Dir.mkdir props[:output]
+	Dir.mkdir props[:out]
 	Dir.mkdir props[:artifacts]
 end
 
 desc "Cleans, versions, compiles the application and generates build_output/."
-task :compile => [:versioning, :global_version, :build] do
-	copyOutputFiles File.join(props[:src], "Bitlore/bin/Release"), "Bitlore.{dll,pdb,xml}", File.join(props[:output], 'net-portable')
+task :compile => [:versioning, :asmver, :nuget_restore, :build] do
+	copyOutputFiles File.join(props[:src], "Bitlore/bin/Release"), "Bitlore.{dll,pdb,xml}", File.join(props[:out], 'net-portable')
 end
 
-desc "Only compiles the application."
-msbuild :build do |msb|
-	msb.properties :Configuration => "Release",
-		:Platform => 'Any CPU',
-    :TargetFrameworkVersion => "v4.0",
-    :SignAssembly => 'true',
-    :AssemblyOriginatorKeyFile => props[:keyfile]
-	msb.use :net4
-	msb.targets :Clean, :Build
-	msb.solution = 'src/Bitlore.sln'
+desc 'create assembly info'
+asmver :asmver => :versioning do |a|
+  a.file_path = File.join(props[:src],'SolutionVersion.cs')
+  a.attributes assembly_description: DESCRIPTION,
+               assembly_configuration: Configuration,
+               assembly_copyright: COPYRIGHT,
+               assembly_version: ENV['FORMAL_VERSION'],
+               assembly_file_version: ENV['FORMAL_VERSION'],
+               assembly_informational_version: ENV['BUILD_VERSION']
 end
+
+build :build do |b|
+  b.file = File.join(props[:src],'Bitlore.sln')
+  b.prop 'Configuration', Configuration
+end
+
+desc "Runs unit tests"
+test_runner :tests => [:compile] do  |t|
+  t.exe = File.join(props[:src], 'packages','xunit.runners.1.9.2', 'tools', 'xunit.console.clr4.exe')
+  t.add_parameter "/xml" 
+  t.add_parameter "#{File.join(props[:artifacts], 'xunit-test-results.xml')}"
+  t.files = FileList[File.join(props[:src], "Bitlore.Tests/bin/#{Configuration}", "Bitlore.Tests.dll")]
+end
+
+task :package => [:create_nuget]
+
+desc "restores missing packages"
+task :nuget_restore do 
+  solution = File.join(props[:src],'Bitlore.sln')
+  sh "src/.nuget/nuget.exe restore #{solution}"
+end
+
+Albacore::Tasks::Versionizer.new :versioning
+
+desc "Builds the nuget package"
+nugets_pack :create_nuget => [:versioning, :compile] do |p|
+  p.files   = FileList['src/**/*.{csproj,fsproj,nuspec}'].
+    exclude(/Tests/)
+  p.out     = 'build_artifacts'
+  p.exe     = 'src/.nuget/nuget.exe'
+  p.with_metadata do |m|
+    m.id          = 'Bitlore'
+    m.title       = PRODUCT
+    m.description = DESCRIPTION
+    m.authors     = COPYRIGHT
+    m.project_url = 'http://github.com/et1975/Bitlore'
+    m.tags        = ['DDD', 'Validation']
+    m.version     = ENV['NUGET_VERSION']
+  end
+  p.with_package do |p|
+	add_assemblies props[:out], '*.{dll,xml}', p 
+  end
+end
+
+#task :ensure_nuget_key do
+#  raise 'missing env NUGET_KEY value' unless ENV['NUGET_KEY']
+#end
+
+#Albacore::Tasks::Release.new :release,
+#                             pkg_dir: 'build/pkg',
+#                             depend_on: [:create_nugets, :ensure_nuget_key],
+#                             nuget_exe: 'packages/NuGet.CommandLine/tools/NuGet.exe',
+#                             api_key: ENV['NUGET_KEY']
 
 def copyOutputFiles(fromDir, filePattern, outDir)
 	FileUtils.mkdir_p outDir unless exists?(outDir)
@@ -73,93 +112,11 @@ def copyOutputFiles(fromDir, filePattern, outDir)
 	}
 end
 
-desc "Runs unit tests"
-xunit :tests => [:compile] do |xunit|
-    xunit.command = File.join('src', 'packages','xunit.Runners.1.9.2', 'tools', 'xunit.console.clr4.exe')
-    xunit.options = ["/xml #{File.join(props[:artifacts], 'xunit-test-results.xml')}"]
-    xunit.assemblies = FileList[File.join(props[:src], "Bitlore.Tests/bin/Release", "Bitlore.Tests.dll")]
-end
-
-task :package => [:nuget, :zip_output]
-
-desc "ZIPs up the build results."
-zip :zip_output => [:versioning] do |zip|
-	zip.directories_to_zip = [props[:output]]
-	zip.output_file = "Bitlore-#{NUGET_VERSION}.zip"
-	zip.output_path = props[:artifacts]
-end
-
-
-desc "restores missing packages"
-msbuild :nuget_restore do |msb|
-  msb.use :net4
-  msb.targets :RestorePackages
-  msb.solution = 'src/Bitlore.Tests/Bitlore.Tests.csproj'
-end
-
-desc "Builds the nuget package"
-task :nuget => [:versioning, :create_nuspec] do
-	sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/Bitlore.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
-end
-
-task :create_nuspec => [:_nuspec]
-
-nuspec :_nuspec do |nuspec|
-  nuspec.id = 'Bitlore'
-  nuspec.version = NUGET_VERSION
-  nuspec.authors = 'Eugene Tolmachev'
-  nuspec.copyright = COPYRIGHT
-  nuspec.description = 'Bitlore, minimalist validation library for DDD affectionados'
-  nuspec.title = 'Bitlore'
-  nuspec.projectUrl = 'http://github.com/et1975/Bitlore'
-  nuspec.language = "en-US"
-  nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
-  nuspec.requireLicenseAcceptance = "false"
-  nuspec.output_file = File.join(props[:artifacts], 'Bitlore.nuspec')
-  add_files props[:output], 'Bitlore.{dll,pdb,xml}', nuspec
-  nuspec.file(File.join(props[:src], "Bitlore\\**\\*.cs").gsub("/","\\"), "src")
-end
-
-def project_outputs(props)
-	props[:projects].map{ |p| "src/#{p}/bin/#{BUILD_CONFIG}/#{p}.dll" }.
-		concat( props[:projects].map{ |p| "src/#{p}/bin/#{BUILD_CONFIG}/#{p}.exe" } ).
-		find_all{ |path| exists?(path) }
-end
-
-def commit_data
-  begin
-    commit = `git rev-parse --short HEAD`.chomp()[0,6]
-    git_date = `git log -1 --date=iso --pretty=format:%ad`
-    commit_date = DateTime.parse( git_date ).strftime("%Y-%m-%d %H%M%S")
-  rescue Exception => e
-    puts e.inspect
-    commit = (ENV['BUILD_VCS_NUMBER'] || "000000")[0,6]
-    commit_date = Time.new.strftime("%Y-%m-%d %H%M%S")
-  end
-  [commit, commit_date]
-end
-
-task :versioning do
-  ver = SemVer.find
-  revision = (ENV['BUILD_NUMBER'] || ver.patch).to_i
-  var = SemVer.new(ver.major, ver.minor, revision, ver.special)
-  
-  # extensible number w/ git hash
-  ENV['BUILD_VERSION'] = BUILD_VERSION = ver.format("%M.%m.%p%s") + ".#{commit_data()[0]}"
-  
-  # nuget (not full semver 2.0.0-rc.1 support) see http://nuget.codeplex.com/workitem/1796
-  ENV['NUGET_VERSION'] = NUGET_VERSION = ver.format("%M.%m.%p%s")
-  
-  # purely M.m.p format
-  ENV['FORMAL_VERSION'] = FORMAL_VERSION = "#{ SemVer.new(ver.major, ver.minor, revision).format "%M.%m.%p"}"
-  puts "##teamcity[buildNumber '#{BUILD_VERSION}']" # tell teamcity our decision
-end
-
-def add_files stage, what_dlls, nuspec
+def add_assemblies stage, what_dlls, nuspec
   [['portable-win+net40+sl40+wp', 'net-portable']].each{|fw|
     takeFrom = File.join(stage, fw[1], what_dlls)
     Dir.glob(takeFrom).each do |f|
-      nuspec.file(f.gsub("/", "\\"), "lib\\#{fw[0]}")
+      nuspec.add_file f.gsub("/", "\\"), "lib\\#{fw[0]}"
     end
   }
 end
